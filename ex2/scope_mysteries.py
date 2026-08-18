dbd81d3c8e63adbe279544d7a89d486a7e28e6fe
peladno <j.perezurrutia@gmail.com>
2026-08-18 18:54:38 +0900

    ex2 on the way

--- ex2/scope_mysteries.py
+++ ex2/scope_mysteries.py
@@ -1,9 +1,30 @@
-
+#!/usr/bin/env python3
 from typing import Callable
 
 
-def mage_counter() -> Callable:
-    
-# def spell_accumulator(initial_power: int) -> Callable
-# def enchantment_factory(enchantment_type: str) -> Callable
-# def memory_vault() -> dict[str, Callable]
+def mage_counter() -> Callable[[], int]:
+    count: int = 1
+
+    def counter() -> int:
+        nonlocal count
+        count += 1
+        return count
+    return counter
+
+
+def spell_accumulator(initial_power: int) -> Callable[[int], int]:
+    def acc(add: int) -> int:
+        return add + initial_power
+    return acc
+
+
+def enchantment_factory(enchantment_type: str) -> Callable[[str], str]:
+    def formatting(item_name: str) -> str:
+        return f"{item_name} {enchantment_type}"
+    return formatting
+
+
+def memory_vault() -> dict[str, Callable:
+    vault = {}
+
+    return store
